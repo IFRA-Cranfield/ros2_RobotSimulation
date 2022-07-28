cd0919dab0f7c5bcc2f0ca6449e7d9cc6b7d1271
IFRA-Cranfield <IFRA@cranfield.ac.uk>
2022-07-28 14:47:53 +0100

    Add MoveR, MoveXYZ and MoveYPR to IRB120+EGP64.

--- ABBRobots/IRB120/irb120egp64_ros2_moveit2/launch/irb120egp64_interface.launch.py
+++ ABBRobots/IRB120/irb120egp64_ros2_moveit2/launch/irb120egp64_interface.launch.py
@@ -325,6 +325,30 @@ def generate_launch_description():
         output="screen",
         parameters=[robot_description, robot_description_semantic, kinematics_yaml],
     )
+    # MoveR ACTION:
+    moveR_interface = Node(
+        name="moveR_action",
+        package="irb120egp64_ros2_moveit2",
+        executable="moveR_action",
+        output="screen",
+        parameters=[robot_description, robot_description_semantic, kinematics_yaml],
+    )
+    # MoveXYZ ACTION:
+    moveXYZ_interface = Node(
+        name="moveXYZ_action",
+        package="irb120egp64_ros2_moveit2",
+        executable="moveXYZ_action",
+        output="screen",
+        parameters=[robot_description, robot_description_semantic, kinematics_yaml],
+    )
+    # MoveYPR ACTION:
+    moveYPR_interface = Node(
+        name="moveYPR_action",
+        package="irb120egp64_ros2_moveit2",
+        executable="moveYPR_action",
+        output="screen",
+        parameters=[robot_description, robot_description_semantic, kinematics_yaml],
+    )
 
     return LaunchDescription(
         [
@@ -351,7 +375,10 @@ def generate_launch_description():
                         moveJ_interface,
                         moveG_interface,
                         moveXYZW_interface,
-                        moveL_interface
+                        moveL_interface,
+                        moveR_interface,
+                        moveXYZ_interface,
+                        moveYPR_interface
 
                     ]
                 )
